5cfa53a9ec776d48e71d229e039136b7224a4343
Roberto Rizzo <69984494+Rob-Rizzo@users.noreply.github.com>
2020-11-12 10:50:59 +0000

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,6 +1,4 @@
 # bounce.py
-#
-# Exercise 1.5
 
 height = 100
 bounce = 1
@@ -10,3 +8,6 @@ while bounce <=1:
     height = height*(3/5)
     print('bounce', round(height,4))
     bounce = bounce+1
+
+#
+# Exercise 1.5
